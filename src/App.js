6eaf928527ac42cb2e61673eb9a9daac41d17f72
Martim <martim.pintao@gmail.com>
2023-03-28 14:09:23 +0100

    Create the UI and the functionality of showing a target box on click

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import wallpaper from "./images/wallpaper.jpg";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [isTargetBoxVisible, setIsTargetBoxVisible] = useState(false);
+
+    const [height, setHeight] = useState();
+
+    const [width, setWidth] = useState();
+
+    useEffect(() => {
+        const target = document.querySelector(".target");
+
+        target.style.left = `calc(${width}px - 5vh)`;
+        target.style.top = `calc(${height}px - 5vh)`;
+
+        isTargetBoxVisible
+            ? (target.style.visibility = "visible")
+            : (target.style.visibility = "hidden");
+    });
+
+    function imgClick(e) {
+        setHeight(e.pageY);
+        setWidth(e.pageX);
+
+        isTargetBoxVisible
+            ? setIsTargetBoxVisible(false)
+            : setIsTargetBoxVisible(true);
+
+        console.log(e);
+    }
+
+    return (
+        <div className="App">
+            <div className="topsection">
+                <h1>Where's Waldo?</h1>
+            </div>
+            <img src={wallpaper} alt="" onClick={imgClick}></img>
+            <div className="target">
+                <div className="targetbox"></div>
+                <div className="dropdown">
+                    <p class="dropdownitem">Waldo</p>
+                    <div className="separator"></div>
+                    <p class="dropdownitem">Wenda</p>
+                    <div className="separator"></div>
+                    <p class="dropdownitem">Odlaw</p>
+                    <div className="separator"></div>
+                    <p class="dropdownitem">Wizard</p>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
